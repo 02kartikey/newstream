c8ea46ee2f16a24ceb95a42bc2f4aa1986d2795f
02kartikey <131267699+02kartikey@users.noreply.github.com>
2023-04-19 20:33:35 +0530

    Update tds8a.py

--- tds8a.py
+++ tds8a.py
@@ -1,11 +1,22 @@
 import streamlit as st
-a=st.number_input(input("First Number"))
-b=st.number_input(input("Second Number"))
-c=st.number_input(input("Third Number"))
-if (a>=b) and (a>=c):
-  la=a
-elif (b>=a) and (b>=c):
-  la=b
-else:
-  la=c
-st.write("Largest number", la)
+
+def find_largest_number(num1, num2, num3):
+    largest_num = max(num1, num2, num3)
+    return largest_num
+
+# Streamlit app
+def tds8a():
+    st.title("Find the Largest Number")
+    st.write("Enter three numbers and find the largest among them.")
+
+    num1 = st.number_input("Enter Number 1", step=1)
+    num2 = st.number_input("Enter Number 2", step=1)
+    num3 = st.number_input("Enter Number 3", step=1)
+
+    if st.button("Find Largest"):
+        largest_num = find_largest_number(num1, num2, num3)
+        st.success(f"The largest number is: {largest_num}")
+
+# Run the Streamlit app
+if _name_ == "_main_":
+    tds8a()
